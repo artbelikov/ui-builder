3076e22c98f0b9b4c6be254bdc8580fa1750ef97
Артём Евграфов <abelikov1608@gmail.com>
2017-08-13 12:08:05 +0300

    Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+/**
+ * Angular 2 decorators and services
+ */
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
+import { AppState } from './app.service';
+
+/**
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.component.css'
+  ],
+  templateUrl: './app.template.html'
+})
+export class AppComponent implements OnInit {
+
+  constructor(
+    public appState: AppState
+  ) {}
+
+  public ngOnInit() {
+  }
+
+}
