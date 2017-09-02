7534d37be5cc6fba712bf8b389d46c0be2fbe1a9
Artyom <79111508721@ya.ru>
2017-09-02 17:28:55 +0300

    Element props update

--- src/app/+form/classes/select.class.ts
+++ src/app/+form/classes/select.class.ts
@@ -1,6 +1,14 @@
-import {ElementsTypes, UIElement} from "@app/+form/classes/element.class";
+import { ElementsTypes, UIElement } from '@app/+form/classes/element.class'
 
-export class UISelect extends UIElement{
-    public type = ElementsTypes[ElementsTypes.Select]
-    public options
+export class UISelect extends UIElement {
+  public type = ElementsTypes[ElementsTypes.Select]
+  public options
+
+  public addOption () {
+    this.options = this.options || []
+    this.options.push({
+      value: '',
+      label: ''
+    })
+  }
 }
